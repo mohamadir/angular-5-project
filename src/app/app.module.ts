abdbe31e19e76488185d1eb7ff975b94fcbd1894
mohamadir <mohamdib@gmail.com>
2018-01-19 22:44:26 +0200

    upload files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { Routes, RouterModule } from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import {DataService} from './data.service';
+import { AboutComponent } from './about/about.component';
+import {FormsModule} from '@angular/forms';
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+  
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(
+      routes // <-- debugging purposes only
+    ) 
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
